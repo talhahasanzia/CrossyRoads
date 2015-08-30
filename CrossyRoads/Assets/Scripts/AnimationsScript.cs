1594faca56d80d8df0fb52f607775b8c369d7eac
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-08-30 20:53:33 +0500

    Stuttering Jump Animation

---
+++ CrossyRoads/Assets/Scripts/AnimationsScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationsScript : MonoBehaviour {
+
+
+    Animator anim;
+    public GameObject obj;
+
+   
+	// Use this for initialization
+	void Start () {
+
+
+        anim = gameObject.GetComponent<Animator>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (Bounce.Jump)
+        {
+
+            anim.SetBool("Jump", true);
+            
+
+        }
+        else
+        {
+
+            anim.SetBool("Jump", false);
+        
+        
+        }
+	}
+}
--- CrossyRoads/Assets/Scripts/Bounce.cs
+++ CrossyRoads/Assets/Scripts/Bounce.cs
@@ -6,66 +6,83 @@ public class Bounce : MonoBehaviour {
 
     float LerpTime;
     float CurrentLerpTime;
-    float Pers = 1;
+    float Perc = 1;
 
     Vector3 startPos;
     Vector3 endPos;
 
-	
+    bool fInput;
+    public static bool Jump;
 	// Update is called once per frame
 	void Update () {
-
-       
-
-            if (Pers == 1)
+        Debug.Log("Jump: " + Jump);
+      
+            if (Perc == 1)
             {
 
                 LerpTime = 1;
                 CurrentLerpTime = 0;
-            
+                fInput = true;
+                
             }
-
+        
 
             startPos = gameObject.transform.position;
 
-            if (Input.GetKeyUp(KeyCode.UpArrow) && gameObject.transform.position==endPos)
+            if (Input.GetKeyDown(KeyCode.UpArrow) && gameObject.transform.position == endPos)
             {
-
+                Jump = true;
                 endPos = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
-            
-            
-            
-            }
-            if (Input.GetKeyUp(KeyCode.DownArrow) && gameObject.transform.position == endPos)
-            {
 
+
+
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow) && gameObject.transform.position == endPos)
+            {
+                Jump = true;
                 endPos = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
 
 
 
             }
-            if (Input.GetKeyUp(KeyCode.LeftArrow) && gameObject.transform.position == endPos)
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && gameObject.transform.position == endPos)
             {
+                Jump = true;
 
-                endPos = new Vector3(transform.position.x , transform.position.y, transform.position.z+1);
+                endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
 
 
 
             }
-            if (Input.GetKeyUp(KeyCode.RightArrow) && gameObject.transform.position == endPos)
+            if (Input.GetKeyDown(KeyCode.RightArrow) && gameObject.transform.position == endPos)
             {
-
-                endPos = new Vector3(transform.position.x , transform.position.y, transform.position.z-1);
+                Jump = true;
+                endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
 
 
 
             }
-            CurrentLerpTime += Time.deltaTime*5.5f;
-            Pers = CurrentLerpTime / LerpTime;
+            CurrentLerpTime += Time.deltaTime * 5f;
+            Perc = CurrentLerpTime / LerpTime;
 
-            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, endPos, Pers);
+
+            if (fInput)
+            {
+                gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, endPos, Perc);
+
+                if (Perc > 0.8)
+                {
+
+                    Perc = 1;
+
+                }
+                if (Mathf.Round(Perc) == 1)
+                {
+
+                    Jump = false;
+                }
+            }
         
-
 
 	}
 }
