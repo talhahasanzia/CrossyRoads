4880726f8a4748bb22c623e5ee91b2297c3acc57
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-08-31 00:29:17 +0500

    Direction and camera implemented
    Direction when moved and camera follow implemented

--- CrossyRoads/Assets/Scripts/AnimationsScript.cs
+++ CrossyRoads/Assets/Scripts/AnimationsScript.cs
@@ -34,5 +34,28 @@ public class AnimationsScript : MonoBehaviour {
         
         
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow)  )
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)  )
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)  )
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)  )
+        {
+
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+        }
 	}
 }
