ad617410a18ef56d68811e4068c60ec05211613f
Talha Hasan Zia <talhahasanzia@hotmail.com>
2015-09-01 09:52:54 +0500

    latest

---
+++ CrossyRoads/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGeneration : MonoBehaviour {
+
+    public GameObject Grass;
+    public GameObject Road;
+    public GameObject Water;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
